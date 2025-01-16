9f2f16a49b2a1709cec341db1fd8b384e126a635
khalidak786 <146335163+khalidak786@users.noreply.github.com>
2025-01-16 15:11:22 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,21 @@
-
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('static-cache-v1').then(cache => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/styles.css',
+          '/main.js',
+          // Add other assets here
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  });
